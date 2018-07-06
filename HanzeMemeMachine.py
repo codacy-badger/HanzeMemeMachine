4defd58fc55ddcb3c37e0d2e7925fcc0d48ef79d
Jeroen Jonker <jeroenjonker.av@gmail.com>
2018-07-06 20:03:30 +0200

    Environment variabelen vervangen met dictionary
    Environment variabelen vervangen met een dictionary in de config.py. Dit is makkelijker uit te breiden. Ook versienummer toegevoegd aan de get request. Versienummer hoeft alleen aangepast worden in de config.py

--- HanzeMemeMachine.py
+++ HanzeMemeMachine.py
@@ -1,18 +1,10 @@
-import os
-
-# in config.py moet een enviroment variabele met je eigen reddit bot token
-import self as self
-
-import config
-
+from config import configuratie
 import discord
 from discord.ext import commands
-
 from JsonParser import JsonParser
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix="!")
-
 
 # log voor als de bot geladen is
 @bot.event
@@ -27,4 +19,4 @@ async def on_message(message):
         await bot.send_message(message.channel, "https://i.imgur.com/msv9liC.gif")
 
 
-bot.run(os.environ["HANZEMEMEMACHINETOKEN"])
+bot.run(configuratie["discord_api_sleutel"])
