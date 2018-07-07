4174e954aee1f4a02b9b9c6749861ffe227a616e
Jeroen Jonker <jeroenjonker.av@gmail.com>
2018-07-07 17:55:24 +0200

    Help teksten verbeterd
    De teksten die !help terug gaf wat nuttiger gemaakt. met voorbeelden

--- HanzeMemeMachine.py
+++ HanzeMemeMachine.py
@@ -8,11 +8,14 @@ bot = commands.Bot(command_prefix="!")
 json_parser = JsonParser()
 
 # als je een nieuwe commando maakt zet hem ook in deze dict. Deze wordt uitgelezen door !help
+# typ de commando's volledig in hoofdletters en zet de tekst die de bot leest om in hoofdletters
+# dit is om het hoofdlettergevoelloos te maken.
 commandos = {
     "!HANZEXPERIENCE": "Post het HanzeXperience logo in de chat waar deze is aangevraagd",
     "!VERSIE": "Toont de versienummer van de bot in de chat waar deze is aangevraagd",
     "!MEME": "Geeft een willekeurige meme van HanzeMemes in de chat waar deze is aangevraagd",
-    "!HELP": "Geeft een lijst terug met alle commando's"
+    "!HELP": "Geeft een lijst terug met alle commando's. \n"
+             "Na help kan een commando getypt worden om hier de beschrijving van de krijgen."
 }
 
 
@@ -41,7 +44,9 @@ async def on_message(message):
                 await bot.send_message(message.channel, "- **" + bericht[1] + "**\n" + beschrijving)
             except KeyError:
                 await bot.send_message(message.channel, "- **" + bericht[1] + " ** \nis geen commando! \n"
-                                                                              "Typ !Help voor een lijst met commando's")
+                                                                              "Typ !Help voor een lijst met commando's. \n"
+                                                                              "Of typ !Help ![commando] om uitleg over een commando te krijgen. \n"
+                                                                              "Bijvoorbeeld: !Help !HanzeXperience")
         else:
             commando_string = ""
             for commando in commandos:
